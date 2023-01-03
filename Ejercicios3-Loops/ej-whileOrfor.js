92eb927db56aa6060d8ae4f36ce983ce90e7c60e
David <david.escandon.v@gmail.com>
2023-01-02 23:45:21 -0300

    changed name and while or for file

---
+++ Ejercicios3-Loops/ej-whileOrfor.js
@@ -0,0 +1,16 @@
+// Ejercicio#1 - Encontrar todos los divisores de un número.
+
+// Ejercicio#2 - Determinar si un número ingresado por el
+// usuario en un loop es primo o no, validar que
+// el número ingresado sea mayor o igual a
+// dos.
+
+// Ejercicio#3 - Crear un programa que determine si un
+// número es perfecto o no, (se dice que un
+// número es perfecto si el número es igual a
+// sus divisores, ejemplos 6 = 1 + 2 + 3)
+
+// Ejercicio#4 - Generar los primeros N números
+// primos, donde n es ingresado por el
+// usuario.
+
