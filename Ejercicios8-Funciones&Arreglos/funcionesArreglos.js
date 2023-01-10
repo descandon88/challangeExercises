c477022a56fd67b7b1edc0385deb75eff70d946a
David <david.escandon.v@gmail.com>
2023-01-10 20:54:27 -0300

    promedio Arreglo Dentro Rango

--- Ejercicios8-Funciones&Arreglos/funcionesArreglos.js
+++ Ejercicios8-Funciones&Arreglos/funcionesArreglos.js
@@ -10,11 +10,9 @@ const devolverPromedioArreglo=(arreglo)=> {
         if (arreglo.length !==0) {
             arrayInt+=arreglo[index];  
         } else {
-            arrayInt=0;
-            
-            break;
-        }
-    
+            arrayInt=0;            
+            // break;
+        }    
     }
     if (arrayInt!==0){ 
         promedio = arrayInt/arreglo.length;
@@ -25,7 +23,8 @@ const devolverPromedioArreglo=(arreglo)=> {
     }
   
 };
-Arreglo = [23,3,21];
+// Test de la función
+let Arreglo = [23,3,21];
 devolverPromedioArreglo(Arreglo);
 
 
@@ -33,3 +32,22 @@ devolverPromedioArreglo(Arreglo);
 // dentro de un rango, esta función recibe el arreglo, un punto de
 // partida y uno de termino y devuelve el promedio de los valores dentro
 // del rango especificado
+
+const promedioArregloDentroRango = (max,min,array)=> {
+    let promedio = 0;
+    let arrayInt = 0;
+    let count=0
+    for (let index = 0; index < array.length; index++) {
+        if(array[index]>=min && array[index]<=max){
+            arrayInt+=array[index];
+            count+=1;
+            console.log('Valor del posicion del array es '+array[index]+' con index '+index);
+        }
+        
+    }
+    promedio = arrayInt / count;
+    console.log('El promedio de los valores del arraglo dentro del rango ingresado es: '+ promedio);
+};
+let arreglo = [10,12,13,14,20,31,35,40,42,52,56,59];
+
+promedioArregloDentroRango(40,20,arreglo)
