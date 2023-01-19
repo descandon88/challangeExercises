cddbd81864e4a9158f580c3532a9a1b73d065b7a
David <david.escandon.v@gmail.com>
2023-01-19 00:39:29 -0300

    funcion misma cantidad de elementos y sumarlos

--- 07-Union-Interseccion-Conteo/01-ejercicio.js
+++ 07-Union-Interseccion-Conteo/01-ejercicio.js
@@ -36,3 +36,20 @@ FunctionMismaCantidad(ArrayNumA,ArrayNumB);
 // ● El usuario ingresa dos conjuntos de números separados por coma, si ambos
 // conjuntos tienen la misma cantidad de elementos mostrar un arreglo que
 // contenga la suma de cada elemento. (Ejemplo: [1,2,3] + [2,3,4] = [3,5,7])
+
+let ArryNumA = [5,25,45,21,15]
+let ArryNumB = [6,35,55,32,12]
+
+const FunctionMismaCantidadElementos = (array1,array2) => {
+    let arreglo = [];
+    if (array1.length == array2.length) {
+        for (let index = 0; index < array1.length; index++) {
+            arreglo.push(array1[index]+array2[index]);     
+        }
+     
+        console.log('['+array1+']'+ ' + ' +'[' + array2 +']'+ ' = ' + '['+arreglo+']');
+    }
+    else {console.log('no tienen la misma cantidad')}    
+}
+
+FunctionMismaCantidadElementos(ArryNumA,ArryNumB);
