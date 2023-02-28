d972ce8a6eac75bcad0a654330acb8a228f2152a
David Escandón <david.escandon.v@gmail.com>
2023-02-28 18:04:34 -0300

    fibonacci function

---
+++ varios/algoritmos/naiveSearchAlgorithm.js
@@ -0,0 +1,7 @@
+// Naive Search Algorithm
+// Naive search algorithm is used to find if a string contains a given substring. For example, check if "helloworld" contains the substring "owo".
+
+// First loop on the main string ("helloworld").
+// Run a nested loop on the substring ("owo").
+// If character does not match then break inner loop else keep looping.
+// If inner loop is completed and got a match, then return true else keep the outer loop going.
